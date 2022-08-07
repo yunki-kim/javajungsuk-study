b1a875b5053ce31c75568cd7431e68c380314a25
yunki-kim <skool109@naver.com>
2022-08-07 17:55:43 +0900

    add Chapter02

---
+++ ch02/src/ByteOverflow.java
@@ -0,0 +1,12 @@
+public class ByteOverflow {
+    public static void main(String[] args) {
+        byte b = 0;
+        int i = 0;
+
+        for (int x = 0; x <= 270; x++) {
+            System.out.print(b++);
+            System.out.print("\t");
+            System.out.println(i++);
+        }
+    }
+}
---
+++ ch02/src/SpecialChar.java
@@ -0,0 +1,11 @@
+public class SpecialChar {
+    public static void main(String[] args) {
+        char single = '\'';
+        String dblQuote = "\"Hello\"";
+        String root = "C:\\";
+
+        System.out.println(single);
+        System.out.println(dblQuote);
+        System.out.println(root);
+    }
+}
