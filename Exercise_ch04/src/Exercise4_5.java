459cb05af0561b73dd9e25fd82272201c1d75e3c
Yuki <skool109@gmail.com>
2022-09-15 14:04:39 +0900

    docs: Update Exercise_ch04
    Exercise4_5 ~ 4_6

---
+++ Exercise_ch04/src/Exercise4_5.java
@@ -0,0 +1,29 @@
+/*
+[연습문제 4-5]
+다음의 for문을 while문으로 변경하시오.
+
+public class Exercise4_5 {
+    public static void main(String[] args) {
+        for(int i=0; i<=10; i++) {
+            for(int j=0; j<=i; j++)
+                System.out.print("*");
+        System.out.println();
+        }
+    }//endofmain
+} // end of class
+*/
+public class Exercise4_5 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i <= 10) {
+            int j = 0;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
